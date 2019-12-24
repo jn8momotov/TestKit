d86908f6a7e20453c490e562a9992ee2e7941fb9
Evgeniy Momotov <jn8momotov@gmail.com>
2019-12-24 20:58:58 +0300

    Initial Commit

---
+++ TestKit/TestKit.h
@@ -0,0 +1,19 @@
+//
+//  TestKit.h
+//  TestKit
+//
+//  Created by Евгений on 24/12/2019.
+//  Copyright © 2019 Momotov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestKit.
+FOUNDATION_EXPORT double TestKitVersionNumber;
+
+//! Project version string for TestKit.
+FOUNDATION_EXPORT const unsigned char TestKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestKit/PublicHeader.h>
+
+
